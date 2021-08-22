07601d2f59af88e1f89d8ae590290503aabad628
Maftuh Mashuri <maftuh.mashuri16@mhs.uinjkt.ac.id>
2021-08-22 10:48:41 +0700

    change optimizer, add hidden layer in fusion model, edit test.py

--- model.py
+++ model.py
@@ -47,21 +47,43 @@ class ResAttCheXNet(nn.Module):
 
 
 class FusionNet(nn.Module):
-    def __init__(self, backbone = 'resnet50', num_classes = 14, **kwargs):
+    def __init__(self, backbone = 'resnet50', num_classes = 14, add_layer = False, **kwargs):
 
         super(FusionNet, self).__init__()
 
         # ----------------- Backbone -----------------
+
+        self.add_layer = add_layer
+
         if backbone == 'resnet50':
-            self.fc = nn.Linear(2048 * 2, num_classes)
+            len_input = 2048
         elif backbone == 'densenet121':
-            self.fc = nn.Linear(1024 * 2, num_classes)
+            len_input = 1024
         else:
             raise Exception("backbone must be resnet50 or densenet121")
+
+        if add_layer:
+            self.fc = nn.Linear(len_input * 2, len_input // 2)
+            self.fc1 = nn.Linear(len_input // 2, len_input // 4)
+            self.fc2 = nn.Linear(len_input // 4, len_input // 8)
+            self.fc3 = nn.Linear(len_input // 8, num_classes)
+            self.relu = nn.ReLU()
+        else:
+            self.fc = nn.Linear(len_input * 2, num_classes)
 
         self.sigmoid = nn.Sigmoid()
 
     def forward(self, pool):
         out = self.fc(pool)
+
+        if self.add_layer:
+            out = self.relu(out)
+            out = self.fc1(out)
+            out = self.relu(out)
+            out = self.fc2(out)
+            out = self.relu(out)
+            out = self.fc3(out)
+
         out = self.sigmoid(out)
+
         return {'out': out}
--- test.py
+++ test.py
@@ -25,6 +25,7 @@ def parse_args():
 	parser.add_argument("--exp_dir", type=str, default='./final_experiments', help='define experiment directory (ex: /exp16)')
 	parser.add_argument("--exp_num", type=str, default='exp0', help='define experiment directory (ex: /exp0)')
 	parser.add_argument("--best_model", "-b", action="store_true")
+	parser.add_argument("--branch", type=str, default='all', help='define branch global or all')
 	args = parser.parse_args()
 	return args
 
@@ -38,14 +39,14 @@ with open(os.path.join(args.exp_dir, "cfg_train.json")) as f:
 	exp_configs = json.load(f)
 	exp_config = exp_configs[args.exp_num]
 
-del exp_configs[exp_config['net']]['branch']
-
-if exp_config['net'] != '?':
-	global_branch_exp = exp_config['net']
-	global_branch = exp_configs[exp_config['net']]
-	exp_config.update(global_branch)
-else:
-	raise Exception("experiment number global branch must be choosen")
+if args.branch == 'all':
+	del exp_configs[exp_config['net']]['branch']
+	if exp_config['net'] != '?':
+		global_branch_exp = exp_config['net']
+		global_branch = exp_configs[exp_config['net']]
+		exp_config.update(global_branch)
+	else:
+		raise Exception("experiment number global branch must be choosen")
 
 config['optimizer'] = {exp_config['optimizer'] : config['optimizer'][exp_config['optimizer']]}
 del exp_config['optimizer']
@@ -60,7 +61,7 @@ if 'num_classes' in list(config.keys()):
 
 NUM_CLASSES = len(CLASS_NAMES)
 
-MAX_BATCH_CAPACITY = 128
+MAX_BATCH_CAPACITY = 32
 
 cudnn.benchmark = True
 device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
@@ -83,51 +84,59 @@ def main():
 
 	# ================= LOAD DATASET ================= #
 
-	test_dataset = ChestXrayDataSet(data_dir = DATA_DIR, split = 'test', transform = transform_test)
+	test_dataset = ChestXrayDataSet(data_dir = DATA_DIR, split = 'test', num_classes = NUM_CLASSES, transform = transform_test)
 	test_loader = DataLoader(dataset = test_dataset, batch_size = MAX_BATCH_CAPACITY, shuffle = False, num_workers = 4, pin_memory = True)
 
 	# ================= MODELS ================= #
 	GlobalModel = ResAttCheXNet(pretrained = False, num_classes = NUM_CLASSES, **config['net']).to(device)
-	LocalModel = ResAttCheXNet(pretrained = False, num_classes = NUM_CLASSES, **config['net']).to(device)
-	AttentionGenPatchs = AttentionMaskInference(threshold = config['threshold'], distance_function = config['L_function'])
-	FusionModel = FusionNet(backbone = config['net']['backbone'], num_classes = NUM_CLASSES).to(device)
+	
+	if args.branch == 'all':
+		LocalModel = ResAttCheXNet(pretrained = False, num_classes = NUM_CLASSES, **config['net']).to(device)
+		AttentionGenPatchs = AttentionMaskInference(threshold = config['threshold'], distance_function = config['L_function'])
+		FusionModel = FusionNet(backbone = config['net']['backbone'], num_classes = NUM_CLASSES).to(device)
 
 	if args.best_model:
 		add_text = '_best'
 	else:
 		add_text = ''
 
-	checkpoint_global = os.path.join(args.exp_dir, global_branch_exp, global_branch_exp + '_global' + add_text + '.pth')
-	checkpoint_local = os.path.join(exp_dir_num, args.exp_num + '_local' + add_text + '.pth')
-	checkpoint_fusion = os.path.join(exp_dir_num, args.exp_num + '_fusion' + add_text + '.pth')
+	checkpoint_global = os.path.join(exp_dir_num, args.exp_num + '_global' + add_text + '.pth')
+	if args.branch == 'all':
+		checkpoint_global = os.path.join(args.exp_dir, global_branch_exp, global_branch_exp + '_global' + add_text + '.pth')
+		checkpoint_local = os.path.join(exp_dir_num, args.exp_num + '_local' + add_text + '.pth')
+		checkpoint_fusion = os.path.join(exp_dir_num, args.exp_num + '_fusion' + add_text + '.pth')
 
 	if os.path.isfile(checkpoint_global):
 		save_dict = torch.load(checkpoint_global)
 		GlobalModel.load_state_dict(save_dict['net'])
 		print(" Loaded Global Branch Model checkpoint from epoch", save_dict['epoch'])
 
-	if os.path.isfile(checkpoint_local):
-		save_dict = torch.load(checkpoint_local)
-		LocalModel.load_state_dict(save_dict['net'])
-		print(" Loaded Local Branch Model checkpoint from epoch", save_dict['epoch'])
+	if args.branch == 'all':
+		if os.path.isfile(checkpoint_local):
+			save_dict = torch.load(checkpoint_local)
+			LocalModel.load_state_dict(save_dict['net'])
+			print(" Loaded Local Branch Model checkpoint from epoch", save_dict['epoch'])
 
-	if os.path.isfile(checkpoint_fusion):
-		save_dict = torch.load(checkpoint_fusion)
-		FusionModel.load_state_dict(save_dict['net'])
-		print(" Loaded Fusion Branch Model checkpoint from epoch", save_dict['epoch'])
+		if os.path.isfile(checkpoint_fusion):
+			save_dict = torch.load(checkpoint_fusion)
+			FusionModel.load_state_dict(save_dict['net'])
+			print(" Loaded Fusion Branch Model checkpoint from epoch", save_dict['epoch'])
 
 	write_csv(os.path.join(exp_dir_num, args.exp_num + '_AUROCs.csv'),
 						data = ['Model'] + CLASS_NAMES + ['Mean'],
 						mode = 'w')
 
 	GlobalModel.eval()
-	LocalModel.eval()
-	FusionModel.eval()
+
+	if args.branch == 'all':
+		LocalModel.eval()
+		FusionModel.eval()
 
 	ground_truth = torch.FloatTensor()
 	pred_global = torch.FloatTensor()
-	pred_local = torch.FloatTensor()
-	pred_fusion = torch.FloatTensor()
+	if args.branch == 'all':
+		pred_local = torch.FloatTensor()
+		pred_fusion = torch.FloatTensor()
 
 	progressbar = tqdm(range(len(test_loader)))
 
@@ -135,18 +144,21 @@ def main():
 	for i, (image, target) in enumerate(test_loader):
 		# compute output
 		output_global = GlobalModel(image.to(device))
-		
-		output_patches = AttentionGenPatchs(image.detach(), output_global['features'].detach().cpu())
 
-		output_local = LocalModel(output_patches['crop'].to(device))
+		if args.branch == 'all':
+			output_patches = AttentionGenPatchs(image.detach(), output_global['features'].detach().cpu())
 
-		pool = torch.cat((output_global['pool'], output_local['pool']), dim = 1)
-		output_fusion = FusionModel(pool.to(device))
+			output_local = LocalModel(output_patches['crop'].to(device))
+
+			pool = torch.cat((output_global['pool'], output_local['pool']), dim = 1)
+			output_fusion = FusionModel(pool.to(device))
 
 		ground_truth = torch.cat((ground_truth, target.detach()), 0)
 		pred_global = torch.cat((pred_global.detach(), output_global['out'].detach().cpu()), 0)
-		pred_local = torch.cat((pred_local.detach(), output_local['out'].detach().cpu()), 0)
-		pred_fusion = torch.cat((pred_fusion.detach(), output_fusion['out'].detach().cpu()), 0)
+
+		if args.branch == 'all':
+			pred_local = torch.cat((pred_local.detach(), output_local['out'].detach().cpu()), 0)
+			pred_fusion = torch.cat((pred_fusion.detach(), output_fusion['out'].detach().cpu()), 0)
 
 		# if (i + 1) % 300 == 0:
 		# 	draw_image = drawImage(image, target, output_fusion.detach().cpu(), image_patch.detach(), heatmaps, coordinates)
@@ -154,26 +166,34 @@ def main():
 
 		progressbar.update(1)
 
-	print(" Testing over time {}:".format(datetime.now() - start_time_test))
-
 	progressbar.close()
+
+	print(" Testing over time {}:".format(datetime.now() - start_time_test))
 
 	AUROCs_global = compute_AUCs(ground_truth, pred_global)
 	AUROCs_global_avg = np.array(AUROCs_global).mean()
-	AUROCs_local = compute_AUCs(ground_truth, pred_local)
-	AUROCs_local_avg = np.array(AUROCs_local).mean()
-	AUROCs_fusion = compute_AUCs(ground_truth, pred_fusion)
-	AUROCs_fusion_avg = np.array(AUROCs_fusion).mean()
+
+	AUROCs_local = [0. for a in range(NUM_CLASSES)]
+	AUROCs_local_avg = 0.
+	AUROCs_fusion = [0. for a in range(NUM_CLASSES)]
+	AUROCs_fusion_avg = 0.
+
+	if args.branch == 'all':
+		AUROCs_local = compute_AUCs(ground_truth, pred_local)
+		AUROCs_local_avg = np.array(AUROCs_local).mean()
+		AUROCs_fusion = compute_AUCs(ground_truth, pred_fusion)
+		AUROCs_fusion_avg = np.array(AUROCs_fusion).mean()
 
 	write_csv(os.path.join(exp_dir_num, args.exp_num + '_AUROCs.csv'),
 						data = ['Global'] + list(AUROCs_global) + [AUROCs_global_avg],
 						mode = 'a')
-	write_csv(os.path.join(exp_dir_num, args.exp_num + '_AUROCs.csv'),
-						data = ['Local'] + list(AUROCs_local) + [AUROCs_local_avg],
-						mode = 'a')
-	write_csv(os.path.join(exp_dir_num, args.exp_num + '_AUROCs.csv'),
-						data = ['Fusion'] + list(AUROCs_fusion) + [AUROCs_fusion_avg],
-						mode = 'a')
+	if args.branch == 'all':
+		write_csv(os.path.join(exp_dir_num, args.exp_num + '_AUROCs.csv'),
+							data = ['Local'] + list(AUROCs_local) + [AUROCs_local_avg],
+							mode = 'a')
+		write_csv(os.path.join(exp_dir_num, args.exp_num + '_AUROCs.csv'),
+							data = ['Fusion'] + list(AUROCs_fusion) + [AUROCs_fusion_avg],
+							mode = 'a')
 
 	print("|===============================================================================================|")
 	print("|\t\t\t\t\t    AUROC\t\t\t\t\t\t|")
@@ -181,7 +201,7 @@ def main():
 	print("|\t\t\t|  Global branch\t|  Local branch\t\t|  Fusion branch\t|")
 	print("|-----------------------------------------------------------------------------------------------|")
 
-	for i in range(len(CLASS_NAMES)):
+	for i in range(NUM_CLASSES):
 		if len(CLASS_NAMES[i]) < 6:
 			print("| {}\t\t\t|".format(CLASS_NAMES[i]), end="")
 		elif len(CLASS_NAMES[i]) > 14:
