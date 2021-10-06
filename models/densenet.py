add mean std normalization for image size 256, and add experiment train cfg, and fix error

models/densenet.py
@@ -142,17 +142,17 @@ class DenseNet(nn.Module):
 
     def __init__(self, growth_rate=32, block_config=(6, 12, 24, 16),
                  num_init_features=64, bn_size=4, drop_rate=0, num_classes=1000, memory_efficient=False,
-                 last_pool = 'lse', lse_pool_controller = 10, one_channel = True):
+                 last_pool = 'lse', lse_pool_controller = 10, is_grayscale = True):
 
         super(DenseNet, self).__init__()
 
-        num_channel = 3
-        if one_channel:
-            num_channel = 1
+        in_channel = 3
+        if is_grayscale:
+            in_channel = 1
 
         # First convolution
         self.features = nn.Sequential(OrderedDict([
-            ('conv0', nn.Conv2d(num_channel, num_init_features, kernel_size=7, stride=2,
+            ('conv0', nn.Conv2d(in_channel, num_init_features, kernel_size=7, stride=2,
                                 padding=3, bias=False)),
             ('norm0', nn.BatchNorm2d(num_init_features)),
             ('relu0', nn.ReLU(inplace=True)),
@@ -216,9 +216,9 @@ class DenseNet(nn.Module):
         out = torch.sigmoid(out)
         return out, features, flatten_pool
 
-def DenseNet121(pretrained = True, num_classes = 14, one_channel = True, last_pool = 'lse', lse_pool_controller = 10, **kwargs):
+def DenseNet121(pretrained = True, num_classes = 14, is_grayscale = True, last_pool = 'lse', lse_pool_controller = 10, **kwargs):
     model = DenseNet(growth_rate = 32, block_config = (6, 12, 24, 16),
-                    num_init_features = 64, num_classes = num_classes, one_channel = one_channel,
+                    num_init_features = 64, num_classes = num_classes, is_grayscale = is_grayscale,
                     last_pool = last_pool, lse_pool_controller = lse_pool_controller, **kwargs)
     if pretrained:
         print(" Loading state dict from", model_urls['densenet121'])
@@ -231,7 +231,7 @@ def DenseNet121(pretrained = True, num_classes = 14, one_channel = True, last_po
 
         state_dict = load_state_dict_from_url(model_urls['densenet121'], progress=True)
 
-        if one_channel:
+        if is_grayscale:
             state_dict['features.conv0.weight'] = state_dict['features.conv0.weight'].mean(axis=1, keepdim = True)
 
         for key in list(state_dict.keys()):

models/resnet.py
@@ -126,7 +126,7 @@ class ResNet(nn.Module):
 
     def __init__(self, block, layers, num_classes=1000, zero_init_residual=False,
                  groups=1, width_per_group=64, replace_stride_with_dilation=None,
-                 norm_layer=None, last_pool = 'lse', lse_pool_controller = 10, one_channel = True):
+                 norm_layer=None, last_pool = 'lse', lse_pool_controller = 10, is_grayscale = True):
         super(ResNet, self).__init__()
         if norm_layer is None:
             norm_layer = nn.BatchNorm2d
@@ -144,11 +144,11 @@ class ResNet(nn.Module):
         self.groups = groups
         self.base_width = width_per_group
 
-        num_channel = 3
-        if one_channel:
-            num_channel = 1
+        in_channel = 3
+        if is_grayscale:
+            in_channel = 1
 
-        self.conv1 = nn.Conv2d(num_channel, self.inplanes, kernel_size=7, stride=2, padding=3,
+        self.conv1 = nn.Conv2d(in_channel, self.inplanes, kernel_size=7, stride=2, padding=3,
                                bias=False)
         self.bn1 = norm_layer(self.inplanes)
         self.controllerelu = nn.ReLU(inplace=True)
@@ -237,14 +237,14 @@ class ResNet(nn.Module):
         return self._forward_impl(x)
 
 
-def ResNet50(pretrained = True, num_classes = 14, one_channel = True, last_pool = 'lse', lse_pool_controller = 10, group_norm = False, **kwargs):
+def ResNet50(pretrained = True, num_classes = 14, is_grayscale = True, last_pool = 'lse', lse_pool_controller = 10, group_norm = False, **kwargs):
 
     if group_norm:
         norm_layer = lambda x: nn.GroupNorm(32, x)
     else:
         norm_layer = None
 
-    model = ResNet(block = Bottleneck, layers = [3, 4, 6, 3], num_classes = num_classes, one_channel = one_channel,
+    model = ResNet(block = Bottleneck, layers = [3, 4, 6, 3], num_classes = num_classes, is_grayscale = is_grayscale,
                     last_pool = last_pool, lse_pool_controller = lse_pool_controller, norm_layer = norm_layer, **kwargs)
 
     if pretrained:
@@ -256,7 +256,7 @@ def ResNet50(pretrained = True, num_classes = 14, one_channel = True, last_pool 
 
         # loaded_state_dict['fc.weight'], loaded_state_dict['fc.bias'] = reduce_weight_bias(loaded_state_dict['fc.weight'].data, loaded_state_dict['fc.bias'].data, num_classes)
 
-        if one_channel:
+        if is_grayscale:
             loaded_state_dict['conv1.weight'] = loaded_state_dict['conv1.weight'].mean(axis=1, keepdim = True).data
             # loaded_state_dict['conv1.weight'] = (loaded_state_dict['conv1.weight'] ** 2).sum(axis = 1, keepdim=True).sqrt().data
 

test.py
@@ -196,14 +196,14 @@ def main():
 		AUROCs_fusion = compute_AUCs(ground_truth, pred_fusion)
 		AUROCs_fusion_avg = np.array(AUROCs_fusion).mean()
 
-	write_csv(os.path.join(exp_dir_num, args.exp_num + '_AUROCs.csv'),
+	write_csv(os.path.join(exp_dir_num, args.exp_num + '_AUROCs' + add_text + '.csv'),
 						data = ['Global'] + list(AUROCs_global) + [AUROCs_global_avg],
 						mode = 'a')
 	if args.branch == 'all':
-		write_csv(os.path.join(exp_dir_num, args.exp_num + '_AUROCs.csv'),
+		write_csv(os.path.join(exp_dir_num, args.exp_num + '_AUROCs' + add_text + '.csv'),
 							data = ['Local'] + list(AUROCs_local) + [AUROCs_local_avg],
 							mode = 'a')
-		write_csv(os.path.join(exp_dir_num, args.exp_num + '_AUROCs.csv'),
+		write_csv(os.path.join(exp_dir_num, args.exp_num + '_AUROCs' + add_text + '.csv'),
 							data = ['Fusion'] + list(AUROCs_fusion) + [AUROCs_fusion_avg],
 							mode = 'a')
 

train.py
@@ -288,21 +288,24 @@ def main():
 	# ================= TRANSFORMS ================= #
 
 	# normalize = transforms.Normalize(
-	#    mean=[0.485, 0.456, 0.406],
-	#    std=[0.229, 0.224, 0.225]
+	#    mean=torch.tensor([[0.485, 0.456, 0.406]]).mean(1),
+	#    std=torch.tensor([[0.229, 0.224, 0.225]]).mean(1)
 	# )
+	normalize = transforms.Normalize(mean=[0.49886124425113754], std=[0.22925289787072856])
 
 	transform_init = transforms.Resize(tuple(config['dataset']['resize']))
 	transform_train = transforms.Compose(
 	   transforms.RandomResizedCrop(tuple(config['dataset']['crop']), (0.25, 1.0)),
 	   transforms.RandomHorizontalFlip(),
 	   transforms.ToTensor(),
+	   # transforms.Normalize(mean=[0.49886124425113754], std=[0.22925289787072856])
 	   transforms.DynamicNormalize()
 	)
 
 	transform_test = transforms.Compose(
 	   transforms.CenterCrop(tuple(config['dataset']['crop'])),
 	   transforms.ToTensor(),
+	   # transforms.Normalize(mean=[0.49886124425113754], std=[0.22925289787072856])
 	   transforms.DynamicNormalize()
 	)
 

utils/utils.py
@@ -164,10 +164,9 @@ def draw_label_score(target, scores, size = (224, 224)):
 def drawImage(images, target, scores, images_cropped = None, heatmaps = None, coordinates = None):
 	bz, c, h, w = images.shape # batch_size, channel, height, width
 
-	unnormalize = UnNormalize(
-		mean=[0.4979839647692935],
-		std=[0.22962109349599796]
-	)
+	# unnormalize = UnNormalize(mean=[0.4979839647692935], std=[0.22962109349599796]) # ori
+	unnormalize = UnNormalize(mean=[0.49886124425113754], std=[0.22925289787072856]) # resize
+
 	img = torch.empty(0, dtype = images.dtype)
 	img_scores = torch.empty(0, dtype = images.dtype)
 